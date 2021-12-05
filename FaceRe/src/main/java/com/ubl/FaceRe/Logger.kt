e8a41b1bad7a55bde581b232c5226466ca6b67c8
Sushant Gautam <susant.gautam@gmail.com>
2021-12-05 15:09:27 +0545

    rename docs

--- FaceRe/src/main/java/com/ubl/FaceRe/Logger.kt
+++ FaceRe/src/main/java/com/ubl/FaceRe/Logger.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 Shubham Panchal
+ * Copyright 2022 Sushant Gautam
  * Licensed under the Apache License, Version 2.0 (the "License");
  * You may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
